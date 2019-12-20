1404d89f6e369bc2c66194353145ba0788e9a98c
unknown <andreivladcristian98@gmail.com>
2019-12-20 21:34:32 +0200

    initial commit

---
+++ SITE/Controllers/AdminController.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SITE.Models;
+using PagedList;
+
+namespace SITE.Controllers
+{
+    public class AdminController : Controller
+    {
+        Database1Entities db = new Database1Entities(); //creez conex bd
+        // GET: Admin
+        [HttpGet] //tipul metodei
+        public ActionResult login()
+        {
+            return View(); //returneaza pagina login
+        }
+
+
+        [HttpPost]
+        public ActionResult login(tabeladmin avm) //se apeleaza cand dau login sau ceva
+        {
+            tabeladmin ad = db.tabeladmins.Where(x => x.ad_username == avm.ad_username && x.ad_password == avm.ad_password).SingleOrDefault();
+            if (ad != null)
+            {
+
+                Session["ad_id"] = ad.ad_id.ToString(); //verifica logarea
+                return RedirectToAction("Create");
+
+            }
+            else
+            {
+                ViewBag.error = "Invalid username or password";
+
+            }
+
+            return View();
+        }
+
+
+        public ActionResult Create()
+        {
+            if (Session["ad_id"] == null)
+            {
+                return RedirectToAction("login");
+            }
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Create(tabelcategory cvm, HttpPostedFileBase imgfile)
+        {
+            string path = uploadimgfile(imgfile);
+            if (path.Equals("-1"))
+            {
+                ViewBag.error = "Image could not be uploaded....";
+            }
+            else
+            {
+                tabelcategory cat = new tabelcategory();
+                cat.cat_name = cvm.cat_name;
+                cat.cat_image = path;
+                cat.cat_status = 1;
+                cat.cat_fk_ad = Convert.ToInt32(Session["ad_id"].ToString());
+                db.tabelcategories.Add(cat);
+                db.SaveChanges();
+                return RedirectToAction("ViewCategory");
+            }
+
+            return View();
+        } 
+
+
+
+        public ActionResult ViewCategory(int? page)
+        {
+            int pagesize = 9, pageindex = 1;
+            pageindex = page.HasValue ? Convert.ToInt32(page) : 1;
+            var list = db.tabelcategories.OrderByDescending(x => x.cat_id).ToList();
+            IPagedList<tabelcategory> stu = list.ToPagedList(pageindex, pagesize);
+
+
+            return View(stu);
+
+
+
+
+        }
+
+        public ActionResult Delete(int? id)
+        {
+
+            tabelcategory p = db.tabelcategories.Where(x => x.cat_id == id).SingleOrDefault();
+            db.tabelcategories.Remove(p);
+            db.SaveChanges();
+
+            return RedirectToAction("ViewCategory");
+        }
+
+
+
+
+        public string uploadimgfile(HttpPostedFileBase file)
+        {
+            Random r = new Random();
+            string path = "-1";
+            int random = r.Next();
+            if (file != null && file.ContentLength > 0)
+            {
+                string extension = Path.GetExtension(file.FileName);
+                if (extension.ToLower().Equals(".jpg") || extension.ToLower().Equals(".jpeg") || extension.ToLower().Equals(".png"))
+                {
+                    try
+                    {
+
+                        path = Path.Combine(Server.MapPath("~/Content/upload"), random + Path.GetFileName(file.FileName));
+                        file.SaveAs(path);
+                        path = "~/Content/upload/" + random + Path.GetFileName(file.FileName);
+
+                       
+                    }
+                    catch (Exception ex)
+                    {
+                        path = "-1";
+                    }
+                }
+                else
+                {
+                    Response.Write("<script>alert('Only jpg ,jpeg or png formats are acceptable....'); </script>");
+                }
+            }
+
+            else
+            {
+                Response.Write("<script>alert('Please select a file'); </script>");
+                path = "-1";
+            }
+
+
+
+            return path;
+        }
+
+
+
+
+
+
+
+
+    }
+}
---
+++ SQLQuery1.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[tabelproduct] (
+    [pro_id]      INT            IDENTITY (1, 1) NOT NULL,
+    [pro_name]    NVARCHAR (50)  NOT NULL,
+    [pro_image]   NVARCHAR (MAX) NOT NULL,
+    [pro_price]   INT            NULL,
+    [pro_des]     NVARCHAR (MAX) NOT NULL,
+    [pro_fk_cat]  INT            NULL,
+    [pro_fk_user] INT            NULL,
+    PRIMARY KEY CLUSTERED ([pro_id] ASC),
+    FOREIGN KEY ([pro_fk_cat]) REFERENCES [dbo].[tabelcategory] ([cat_id]),
+    FOREIGN KEY ([pro_fk_user]) REFERENCES [dbo].[tabeluser] ([u_id])
+);
